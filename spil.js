45522d99a16a7531630945a7908d7ebc4c5d0b9a
Mathilde Runge Oldorf <MathildeOldorf@inspiron.kea.local>
2017-11-09 15:09:12 +0100

    123456

--- spil.js
+++ spil.js
@@ -2,4 +2,12 @@ $(window).on("load", sidenErLoadet);
 
 function sidenErLoadet() {
     console.log("siden er loadet");
+    $("#billede_container").addClass("druktur_billede");
+    $("#billede_container").addClass("fade");
+    $("#billede_container").on("click", hehe);
+}
+
+function hehe() {
+    console.log("fjernet");
+    $("#billede_container").removeClass("druktur_billede");
 }
