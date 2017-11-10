309548e058465bd76a407cf682e2b54c81b01fe0
Mathilde Runge Oldorf <MathildeOldorf@desktop-37ou6fm.kea.local>
2017-11-10 10:04:23 +0100

    javascript i spil - arrays og tællere

--- spil.js
+++ spil.js
@@ -1,7 +1,16 @@
 $(window).on("load", sidenErLoadet);
 
-var antalKlik = 0;
+var billedeNr = 0;
 var klikket5Billeder = false;
+
+
+
+var billede = ["#klasse_billede", "#druk_billede", "#babysitting_billede", "#toilet_billede", "#ferie_billede"];
+
+var ros = ["ros_klasse", "rose_druk", "ros_babysitting", "ros_toilet", "ros_ferie"];
+
+var info = ["info_klasse", "info_druk", "info_babysitting", "info_toilet", "info_ferie"];
+
 
 
 
@@ -9,118 +18,130 @@ function sidenErLoadet() {
     console.log("siden er loadet - startbillede fader ind");
     $("#start_billede").addClass("fade");
 
-    $("#loginknap").on("click", foersteBillede);
+    $("#loginknap").on("click", visBillede);
 }
+
+
 
 // FØRSTE BILLEDE
 
-function foersteBillede() {
+function visBillede() {
     console.log("Første billede: klassebillede fader ind");
 
-    $("#loginknap").off("click", foersteBillede);
+    $("#loginknap").off("click", visBillede);
 
     $("#start_billede").removeClass("fade");
-
-    $("#klasse_billede").addClass("fade");
+    console.log("billede[billedeNr]:", billede[billedeNr]);
+    $(billeder[billedeNr]).addClass("fade");
     //effekter med knapper skal lægges ind
 
     $("#klasse_billede").on("animationend", tagValg);
+
+
 }
 
 function tagValg() {
     console.log("knapperne pulser");
 
-    $("#klasse_billede").off("animationend", tagValg);
+    //$("#klasse_billede").off("animationend", tagValg);
+
+    $("#delknap").on("click", klikDel);
+    $("#lukknap").on("click", klikLuk);
+
 }
 
-function klikDel1() {
-    console.log("Bruger vælger at dele 1");
+function klikDel() {
+    console.log("Bruger vælger at dele");
 
-    $("#klasse_billede").removeClass("fade");
+    $(billede[billedeNr]).removeClass("fade");
     //remove puls
 
     $("#info1").addClass("fade");
 
     //hvordan skal vi videre herfra? bare on animationend eller skal vi have en knap?
+    billedeNr++;
+    visBillede();
 }
 
-function klikKryds1() {
-    console.log("Bruger vælger IKKE at dele 1");
+function klikLuk() {
+    console.log("Bruger vælger IKKE at dele");
 
-    $("#klasse_billede").removeClass("fade");
+    $(billeder[billedeNr]).removeClass("fade");
     //remove puls
 
     $("#ros1").addClass("fade");
 
     //hvordan skal vi videre herfra? bare on animationend eller skal vi have en knap?
+    billedeNr++;
+    visBillede();
 }
 
-// ANDET BILLEDE
-
-
-function andetBillede() {
-    console.log("Andet billede: druktursbillede fader ind");
-
-    $("#klasse_billede").removeClass("fade");
-
-    $("#druktur_billede").addClass("fade");
-
-    $("druktur_billede").on("animationend", tagValg);
-}
-
-function klikDel2() {
-    console.log("Bruger vælger at dele 2");
-}
-
-function klikKryds2() {
-    console.log("Bruger vælger IKKE at dele 2");
-}
-
-
-//TREDJE BILLEDE
-
-function tredjeBillede() {
-    console.log("Tredje billede: babysitting-billede fader ind");
-    $("#babysitting_billede").on("animationend", tagValg);
-}
-
-function klikDel3() {
-    console.log("Bruger vælger at dele 3");
-}
-
-function klikKryds3() {
-    console.log("Bruger vælger IKKE at dele 3");
-}
-
-
-//FJERDE BILLEDE
-
-function fjerdeBillede() {
-    console.log("Fjerde billede: toilet-billede fader ind");
-}
-
-function klikDel4() {
-    console.log("Bruger vælger at dele 4");
-}
-
-function klikKryds4() {
-    console.log("Bruger vælger IKKE at dele 4");
-}
-
-
-//FEMTE BILLEDE
-
-function femteBillede() {
-    console.log("Femte billede: rejse-billede fader ind");
-}
-
-function klikDel5() {
-    console.log("Bruger vælger at dele 5");
-}
-
-function klikKryds5() {
-    console.log("Bruger vælger IKKE at dele 5");
-}
+//// ANDET BILLEDE
+//
+//
+//function andetBillede() {
+//    console.log("Andet billede: druktursbillede fader ind");
+//
+//    $("#klasse_billede").removeClass("fade");
+//
+//    $("#druktur_billede").addClass("fade");
+//
+//    $("druktur_billede").on("animationend", tagValg);
+//}
+//
+//function klikDel2() {
+//    console.log("Bruger vælger at dele 2");
+//}
+//
+//function klikKryds2() {
+//    console.log("Bruger vælger IKKE at dele 2");
+//}
+//
+//
+////TREDJE BILLEDE
+//
+//function tredjeBillede() {
+//    console.log("Tredje billede: babysitting-billede fader ind");
+//    $("#babysitting_billede").on("animationend", tagValg);
+//}
+//
+//function klikDel3() {
+//    console.log("Bruger vælger at dele 3");
+//}
+//
+//function klikKryds3() {
+//    console.log("Bruger vælger IKKE at dele 3");
+//}
+//
+//
+////FJERDE BILLEDE
+//
+//function fjerdeBillede() {
+//    console.log("Fjerde billede: toilet-billede fader ind");
+//}
+//
+//function klikDel4() {
+//    console.log("Bruger vælger at dele 4");
+//}
+//
+//function klikKryds4() {
+//    console.log("Bruger vælger IKKE at dele 4");
+//}
+//
+//
+////FEMTE BILLEDE
+//
+//function femteBillede() {
+//    console.log("Femte billede: rejse-billede fader ind");
+//}
+//
+//function klikDel5() {
+//    console.log("Bruger vælger at dele 5");
+//}
+//
+//function klikKryds5() {
+//    console.log("Bruger vælger IKKE at dele 5");
+//}
 
 
 //RESULTAT
