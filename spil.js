9557a28cf94988e53eb3c5a12a06437a9f883bd9
Mathilde Runge Oldorf <MathildeOldorf@laptop-b5ls64hv.kea.local>
2017-11-13 14:21:53 +0100

    lyd udok

--- spil.js
+++ spil.js
@@ -20,7 +20,7 @@ var info = ["#info1", "#info2", "#info3", "#info4", "#info5"];
 function sidenErLoadet() {
     console.log("siden er loadet - logincycle begynder");
 
-    $("#taste_lyd")[0].play();
+//    $("#taste_lyd")[0].play();
 
     $("#lukknap").hide();
 
