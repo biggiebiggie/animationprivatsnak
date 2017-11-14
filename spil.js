93f95a29a397e565065dba933c5e54e5f9943da4
Mathilde Runge Oldorf <MathildeOldorf@laptop-go42e3qu.kea.local>
2017-11-14 09:26:41 +0100

    sdfghjk

--- spil.js
+++ spil.js
@@ -6,7 +6,7 @@ var billedeNr = 0;
 
 
 
-var billede = ["#klasse_billede", "#druk_billede", "#babysitting_billede", "#toilet_billede", "#ferie_billede"];
+var billede = ["#klasse_billede", "#druk_billede", "#babysitting_billede", "#rejse_billede", "#ferie_billede"];
 
 var hashtag = ["#hashtag_klassebillede", "#hashtag_druk", "#hashtag_babysitting", "#hashtag_toilet", "#hashtag_ferie"];
 
